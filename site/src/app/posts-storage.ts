029c1f0b26ce3873698879156ba26ac28dfda700
Daniel Hill <dhhillaz@gmail.com>
2019-02-11 14:25:52 -0700

    Improved memory managment

--- site/src/app/posts-storage.ts
+++ site/src/app/posts-storage.ts
@@ -2,15 +2,21 @@ import { Message } from './message';
 
 export const POSTS: Message[] = [
     {
-        title: "example_title",
-        img: "assets/img/Ninja_Runner.gif",
-        txt: "Interesting game eh",
-        link: "nowhere"
+        title: "Genetic Algorithm Maze Solver",
+        img: "assets/video/genetic_maze.webm",
+        txt: "    boring game eh",
+        link: "everywhere"
     },
     {
-        title: "2example_title",
-        img: "assets/img/Penguin_game.gif",
-        txt: "boring game eh",
+        title: "Penguin Game",
+        img: "assets/video/penguin_game.webm",
+        txt: "    boring game eh",
         link: "everywhere"
+    },
+    {
+        title: "Ninja Runner",
+        img: "assets/video/ninja_game.webm",
+        txt: "Interesting game eh",
+        link: "nowhere"
     }
 ];
